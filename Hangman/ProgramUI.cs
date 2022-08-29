f693bb63b1cd4bc393c6620734dba2a57fb6cad9
Clarissa Fields <clarfields2016@gmail.com>
2022-08-29 19:41:09 -0400

    added extra hints, formating in terminal, and tries remaing with body part

--- Hangman/ProgramUI.cs
+++ Hangman/ProgramUI.cs
@@ -6,6 +6,7 @@ public class ProgramUI
     int numInWord = 0;
     bool gameRunning = true;
     int bodyHungCount = 0;
+    int wordNum = 0;
 
     public void Run()
     {
@@ -19,7 +20,7 @@ public class ProgramUI
         string[] words = { "cherry", "apple", "blueberry", "pumpkin", "peanutbutter", "rhubarb" };
 
         Random rand = new Random();
-        int wordNum = rand.Next(0, 6); // .Next(min int{included in range}, max int{NOT included in range} )
+        wordNum = rand.Next(0, 6); // .Next(min int{included in range}, max int{NOT included in range} )
 
         string gameWord = words[wordNum];
 
@@ -41,7 +42,7 @@ public class ProgramUI
         while (gameRunning)
         {
 
-            System.Console.WriteLine("\nGuess a letter: ");
+            System.Console.WriteLine("\n\nGuess a letter: ");
             string userGuess = Console.ReadLine().ToLower();
 
             CheckForLetter(userGuess, gameArr);
@@ -51,43 +52,49 @@ public class ProgramUI
 
     public void DisplayBody(int hangmanBody)
     {
+        int triesLeft = 6 - hangmanBody;
+
+        string[] sHints1 = { "I am red.", "I am typically made with cinnamon.", "I'm a beatboxing ______.", "I am normally made during the last few months of the year.", "You can have me on a sandwitch.", "I am almost always cooked with lots of sugar because I am very sour." };
+
         switch (hangmanBody)
         {
             case 1:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Head added");
+                System.Console.WriteLine($"Head added, {triesLeft} tries remaining.\n");
                 Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 2:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Torso added");
+                System.Console.WriteLine($"Torso added, {triesLeft} tries remaining.\n");
                 Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine("(hint: types of pie)\n");
                 break;
             case 3:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Left arm added");
+                System.Console.WriteLine($"Left arm added, {triesLeft} tries remaining.\n");
                 Console.ForegroundColor = ConsoleColor.White;
-                System.Console.WriteLine("\n(hint: types of pie)\n");
                 break;
             case 4:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Right arm added");
+                System.Console.WriteLine($"Right arm added, {triesLeft} tries remaining.\n");
                 Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine($"Hint two: {sHints1[wordNum]} \n");
                 break;
             case 5:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Left leg added");
+                System.Console.WriteLine($"Left leg added, {triesLeft} tries remaining.\n");
                 Console.ForegroundColor = ConsoleColor.White;
                 break;
             case 6:
                 Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Right leg added");
+                System.Console.WriteLine($"Right leg added, {triesLeft} tries remaining.\n");
+                System.Console.WriteLine("Game Over...\n");
+                for (int i = 0; i < gameArr.Length; i++)
+                {
+                    System.Console.Write($"{gameArr[i]} ");
+                }
                 Console.ForegroundColor = ConsoleColor.White;
-                break;
-            case 7:
-                Console.ForegroundColor = ConsoleColor.Red;
-                System.Console.WriteLine("Game Over...");
-                Console.ForegroundColor = ConsoleColor.White;
+                System.Console.WriteLine(" was the correct word.");
                 gameRunning = false;
                 break;
             default:
@@ -138,7 +145,7 @@ public class ProgramUI
 
             BlankDisplay();
             Console.ForegroundColor = ConsoleColor.Green;
-            System.Console.WriteLine("\nCongrats you got the word!");
+            System.Console.WriteLine("\n\nCongrats you got the word!\n");
             Console.ForegroundColor = ConsoleColor.White;
             gameRunning = false;
         }
@@ -147,16 +154,28 @@ public class ProgramUI
             BlankDisplay();
         }
     }
+    // public void HangmanDisplay()
+    // {
+    //     System.Console.WriteLine(@$"
+    //             ________
+    //            |        |
+    //            |        |
+    //            {h}        |
+    //          {ra}{b}{la}        |
+    //            {b}        |
+    //         {rl} {ll}        |
+    //                 ____|____");
+    // }
     public void CorrectAnswer()
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        System.Console.WriteLine("Correct Guess!");
+        System.Console.WriteLine("Correct Guess!\n");
         Console.ForegroundColor = ConsoleColor.White;
     }
     public void IncorrectAnswer()
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        System.Console.WriteLine("Sorry! wrong answer");
+        System.Console.WriteLine("Sorry! Wrong guess\n");
         Console.ForegroundColor = ConsoleColor.White;
         bodyHungCount++;
         DisplayBody(bodyHungCount);
